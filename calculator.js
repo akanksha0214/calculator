1fa380a37c7a2d7a94813fe99bc9125e04d1ac36
akanksha0214 <98308651+akanksha0214@users.noreply.github.com>
2024-04-01 11:33:06 +0530

    Update calculator.js

---
+++ calculator.js
@@ -0,0 +1,19 @@
+
+function setValue(val){
+    document.querySelector(".inp").value+=val;
+}
+function calculate(){
+    var data=document.querySelector(".inp").value;
+    let answer=eval(data);
+    document.querySelector(".inp").value=answer;
+}
+
+function clearAll(){
+    document.querySelector(".inp").value="";
+}
+
+function backSpace(){
+    let data=document.querySelector(".inp").value;
+    let newData=data.substring(0,data.length-1);
+    document.querySelector(".inp").value=newData;
+  
